8901e4cd4e1a282d8331aa16d0e422f4b4fe5ca9
toradziro <toradziro@gmail.com>
2025-03-04 22:24:08 +0100

    define radiance and depth

--- sandbox/src/main.cpp
+++ sandbox/src/main.cpp
@@ -2,8 +2,11 @@
 #include <format>
 
 #include <GLFW/glfw3.h>
-#include <glm/glm.hpp>
 #include <vulkan/vulkan.h>
+
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
 
 int main(int argc, char** argv)
 {
