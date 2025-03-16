ec7839b4cb5082772b416a72029026db0b8f641e
toradziro <toradziro@gmail.com>
2025-03-16 12:16:02 +0100

    create logical device

--- sandbox/src/renderer/renderer.cpp
+++ sandbox/src/renderer/renderer.cpp
@@ -9,10 +9,12 @@ void Renderer::init(GLFWwindow* window)
 
 	createVkInstance();
 	setupPhysicalDevice();
+	createLogicalDevice();
 }
 
 void Renderer::shutdown()
 {
+	vkDestroyDevice(m_logicalDevice, nullptr);
 	vkDestroyInstance(m_vkInstance, nullptr);
 }
 
@@ -67,7 +69,7 @@ void Renderer::createVkInstance()
 	assert(m_vkInstance != VK_NULL_HANDLE);
 }
 
-void Renderer::checkExtentionsSupport(const std::vector<const char*>& instanceExtentionsAppNeed)
+void Renderer::checkExtentionsSupport(const std::vector<const char*>& instanceExtentionsAppNeed) const
 {
 	uint32_t extentionsCount = 0;
 	vkEnumerateInstanceExtensionProperties(nullptr, &extentionsCount, nullptr);
@@ -92,6 +94,41 @@ void Renderer::checkExtentionsSupport(const std::vector<const char*>& instanceEx
 	}
 }
 
+void Renderer::createLogicalDevice()
+{
+	//-- Queue for device info
+	VkDeviceQueueCreateInfo queueCreateInfo = {};
+	queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+	queueCreateInfo.queueFamilyIndex = m_physicalDeviceQueueFamilies.m_graphicQueue;
+	queueCreateInfo.queueCount = 1;
+	float priority = 1.0f;
+	queueCreateInfo.pQueuePriorities = &priority;
+
+	//-- Device info itself
+	VkDeviceCreateInfo deviceCreateInfo = {};
+	deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	deviceCreateInfo.queueCreateInfoCount = 1;
+	deviceCreateInfo.pQueueCreateInfos = &queueCreateInfo;
+	deviceCreateInfo.enabledExtensionCount = 0;
+	deviceCreateInfo.ppEnabledExtensionNames = nullptr;
+
+	VkPhysicalDeviceFeatures deviceFeatures = {};
+	deviceCreateInfo.pEnabledFeatures = &deviceFeatures;
+
+	//-- Creating logical device
+	VkResult result = vkCreateDevice(m_physicalDevice
+		, &deviceCreateInfo
+		, nullptr
+		, &m_logicalDevice);
+	assert(result == VK_SUCCESS);
+
+	//-- Queues are created automatically, we save it
+	vkGetDeviceQueue(m_logicalDevice
+		, m_physicalDeviceQueueFamilies.m_graphicQueue
+		, 0
+		, &m_queues.m_graphicQueue);
+}
+
 void Renderer::setupPhysicalDevice()
 {
 	uint32_t deviceCount = 0;
@@ -101,23 +138,79 @@ void Renderer::setupPhysicalDevice()
 	physDevices.resize(deviceCount);
 	vkEnumeratePhysicalDevices(m_vkInstance, &deviceCount, physDevices.data());
 
-	int bestScore = -1;
+	int bestScore = 0;
 	for (VkPhysicalDevice& device : physDevices)
 	{
-		int currScore = 0;
+		//-- Looking for the best device
+		auto [currScore, queueFamilies] = checkIfPhysicalDeviceSuitable(device);
 
-		//-- Prefer discrete videocard
-		VkPhysicalDeviceProperties properties;
-		vkGetPhysicalDeviceProperties(device, &properties);
-		if (properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
-		{
-			currScore += 1;
-		}
 		if (currScore > bestScore)
 		{
 			bestScore = currScore;
 			m_physicalDevice = device;
+			m_physicalDeviceQueueFamilies = queueFamilies;
 		}
 	}
+	assert(bestScore > 0);
 	assert(m_physicalDevice != VK_NULL_HANDLE);
+	assert(m_physicalDeviceQueueFamilies.isValid());
+}
+
+QueueFamilies Renderer::checkQueueFamilies(VkPhysicalDevice device) const
+{
+	QueueFamilies queueFamilies = {};
+
+	uint32_t queueFamilyCount = 0;
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+	assert(queueFamilyCount > 0);
+
+	std::vector<VkQueueFamilyProperties> queueFamilyProps;
+	queueFamilyProps.resize(queueFamilyCount);
+	vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilyProps.data());
+
+	int index = 0;
+	for (auto& familyProp : queueFamilyProps)
+	{
+		//-- Check if queue has graphic family and queue count is not zero
+		if (familyProp.queueCount > 0 && !!(familyProp.queueFlags & VK_QUEUE_GRAPHICS_BIT))
+		{
+			//-- Remember that queue index to work with that later
+			queueFamilies.m_graphicQueue = index;
+		}
+		if (queueFamilies.isValid())
+		{
+			//-- Already found needed queues
+			break;
+		}
+
+		++index;
+	}
+
+	return queueFamilies;
+}
+
+std::pair<int, QueueFamilies> Renderer::checkIfPhysicalDeviceSuitable(VkPhysicalDevice device) const
+{
+	int score = 0;
+
+	VkPhysicalDeviceProperties properties = {};
+	vkGetPhysicalDeviceProperties(device, &properties);
+
+	QueueFamilies queueFamilies = checkQueueFamilies(device);
+	if (queueFamilies.isValid())
+	{
+		score += 10;
+	}
+
+	//-- Prefer discrete videocard but only if it fits by queues reqs
+	if (score > 0 && properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+	{
+		score += 1;
+	}
+
+	//-- Information about what device can do (geom shaders, tess shaders, wide lines, etc)
+	VkPhysicalDeviceFeatures deviceFeatures = {};
+	vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+	return { score, queueFamilies };
 }
--- sandbox/src/renderer/renderer.h
+++ sandbox/src/renderer/renderer.h
@@ -4,6 +4,21 @@
 #include <GLFW/glfw3.h>
 #include <vector>
 
+struct QueueFamilies
+{
+	int	m_graphicQueue = -1;
+
+	bool isValid() const
+	{
+		return m_graphicQueue >= 0;
+	}
+};
+
+struct Queues
+{
+	VkQueue	m_graphicQueue;
+};
+
 class Renderer
 {
 public:
@@ -12,15 +27,21 @@ public:
 	void update(float /*dt*/);
 
 private:
-	void createVkInstance();
-	void checkExtentionsSupport(const std::vector<const char*>& instanceExtentionsAppNeed);
+	void	createVkInstance();
+	void	checkExtentionsSupport(const std::vector<const char*>& instanceExtentionsAppNeed) const;
 
-	void setupPhysicalDevice();
+	void	createLogicalDevice();
+
+	void							setupPhysicalDevice();
+	std::pair<int, QueueFamilies>	checkIfPhysicalDeviceSuitable(VkPhysicalDevice device) const;
+	QueueFamilies					checkQueueFamilies(VkPhysicalDevice device) const;
 
 private:
 	//GLFWwindow*	m_window = nullptr;
 
 	VkInstance			m_vkInstance = VK_NULL_HANDLE;
 	VkPhysicalDevice	m_physicalDevice = VK_NULL_HANDLE;
+	QueueFamilies		m_physicalDeviceQueueFamilies = {};
+	Queues				m_queues = {};
 	VkDevice			m_logicalDevice = VK_NULL_HANDLE;
 };
